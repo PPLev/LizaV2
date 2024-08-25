d3531db749279d9f005751f98ebc1954c14955a2
Admin <sergo.p.987@gmail.com>
2024-08-25 23:26:33 +0300

    add yaml connections rules

--- connection.py
+++ connection.py
@@ -1,8 +1,58 @@
+import os.path
 from dataclasses import dataclass
 from typing import List
+import yaml
+
+from event import Event
+
 
 @dataclass
 class Connection:
-    sender: str
-    allowed_purposes: List[str]
-    acceptor: str
+    name: str
+    senders: List[str]
+    before_ext: List[str]
+    purposes: List[str]
+    post_ext: List[str]
+    acceptors: List[str]
+
+    @staticmethod
+    def load(filename: str) -> List['Connection']:
+        if not os.path.isfile(filename):
+            return []
+
+        with open(filename, 'r') as file:
+            data = yaml.safe_load(file)
+
+        connections = []
+
+        for rule in data["rules"]:
+            connections.append(
+                Connection(
+                    name=rule,
+                    **data["rules"][rule]
+                )
+            )
+
+        for addition in data["includes"]:
+            connections.extend(Connection.load(addition))
+
+        return connections
+
+    def check_event(self, event: Event) -> bool:
+        event_connection_allowed = False
+
+        is_event_purposed = hasattr(event, "purpose")
+        exec_purpose = bool(len(self.purposes))
+
+        if exec_purpose and is_event_purposed:
+            if event.purpose in self.purposes:
+                event_connection_allowed = True
+
+        if not exec_purpose:
+            event_connection_allowed = True
+
+        return event_connection_allowed
+
+
+if __name__ == '__main__':
+    connections = Connection.load('connections/connections.yml')
