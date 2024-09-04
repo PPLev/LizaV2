448e75b4c75ea506e5d3fb03536c722342c6e28d
Admin <sergo.p.987@gmail.com>
2024-09-04 15:28:13 +0300

    добавлено расширение для обрезки json в event.value

--- modules/event_utils/utils.py
+++ modules/event_utils/utils.py
@@ -1,3 +1,5 @@
+import json
+
 from num2words import num2words
 
 from event import Event, EventTypes
@@ -21,5 +23,16 @@ def change_type(event: Event, new_type: str = EventTypes.text):
 def change_purpose(event: Event, new_purpose: str = "none"):
     event.purpose = new_purpose
     return event
+
+
+def json_find(event: Event):
+    text = event.value
+    try:
+        json_data_ = "{" + text.split("{")[1]
+        json_data_ = json_data_.split("}")[0] + "}"
+        event.value = json_data_
+        return event
+    except:
+        return event
 
 # def move_queue(event: Event):
