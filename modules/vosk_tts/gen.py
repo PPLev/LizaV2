fe9177480ac1b31932e625df652eca38c529020a
Admin <sergo.p.987@gmail.com>
2024-09-20 16:46:24 +0300

    Добавление модуля озвучки vosk_tts

--- modules/vosk_tts/gen.py
+++ modules/vosk_tts/gen.py
@@ -1,3 +1,5 @@
+import os.path
+
 from vosk_tts import Model, Synth
 
 from event import Event
@@ -7,5 +9,8 @@ synth = Synth(model)
 
 
 def gen_voice(event: Event):
-    if
-    synth.synth("Привет мир!", f"modules/vosk_tts/{event.value[:20]}.wav", speaker_id=2)
+    file_name = f"modules/vosk_tts/{event.value[:20]}.wav"
+    if not os.path.isfile(file_name):
+        synth.synth(event.value, file_name, speaker_id=2)
+
+    # play_file(file_name)
